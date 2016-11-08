2fb69b56c5c4c61ccdd502e9f8a85a9767433ea0
alexvictoor <alexvictoor@gmail.com>
2016-11-08 22:47:48 +0100

    complete histogram size init

--- src/AbstractHistogram.ts
+++ src/AbstractHistogram.ts
@@ -78,7 +78,7 @@ export abstract class AbstractHistogram extends AbstractHistogramBase {
    * Get the total count of all recorded values in the histogram
    * @return the total count of all recorded values in the histogram
    */
-  public abstract getTotalCount(): number;
+  abstract getTotalCount(): number;
 
   private updatedMaxValue(value: number): void  {
     const internalValue : number = value | this.unitMagnitudeMask;
@@ -153,13 +153,76 @@ export abstract class AbstractHistogram extends AbstractHistogramBase {
       this.subBucketHalfCount = this.subBucketCount / 2;
       this.subBucketMask = (Math.round(this.subBucketCount) - 1) << this.unitMagnitude;
 
-      // TODO....
-      // this.establishSize(highestTrackableValue);
+      this.establishSize(highestTrackableValue);
 
       // TODO was 64 but in JS this should be 53, right?
       this.leadingZeroCountBase = 53 - this.unitMagnitude - this.subBucketHalfCountMagnitude - 1;
       //this.percentileIterator = new PercentileIterator(this, 1);
       //this.recordedValuesIterator = new RecordedValuesIterator(this);
+    }
+
+    /**
+     * The buckets (each of which has subBucketCount sub-buckets, here assumed to be 2048 as an example) overlap:
+     *
+     * <pre>
+     * The 0'th bucket covers from 0...2047 in multiples of 1, using all 2048 sub-buckets
+     * The 1'th bucket covers from 2048..4097 in multiples of 2, using only the top 1024 sub-buckets
+     * The 2'th bucket covers from 4096..8191 in multiple of 4, using only the top 1024 sub-buckets
+     * ...
+     * </pre>
+     *
+     * Bucket 0 is "special" here. It is the only one that has 2048 entries. All the rest have 1024 entries (because
+     * their bottom half overlaps with and is already covered by the all of the previous buckets put together). In other
+     * words, the k'th bucket could represent 0 * 2^k to 2048 * 2^k in 2048 buckets with 2^k precision, but the midpoint
+     * of 1024 * 2^k = 2048 * 2^(k-1) = the k-1'th bucket's end, so we would use the previous bucket for those lower
+     * values as it has better precision.
+     */
+    establishSize(newHighestTrackableValue: number): void {
+        // establish counts array length:
+        this.countsArrayLength = this.determineArrayLengthNeeded(newHighestTrackableValue);
+        // establish exponent range needed to support the trackable value with no overflow:
+        this.bucketCount = this.getBucketsNeededToCoverValue(newHighestTrackableValue);
+        // establish the new highest trackable value:
+        this.highestTrackableValue = newHighestTrackableValue;
+    }
+
+    determineArrayLengthNeeded(highestTrackableValue: number): number {
+        if (highestTrackableValue < 2 * this.lowestDiscernibleValue) {
+            throw new Error("highestTrackableValue (" + highestTrackableValue +
+                    ") cannot be < (2 * lowestDiscernibleValue)");
+        }
+        //determine counts array length needed:
+        const countsArrayLength = this.getLengthForNumberOfBuckets(this.getBucketsNeededToCoverValue(highestTrackableValue));
+        return countsArrayLength;
+    }
+
+    /**
+     * If we have N such that subBucketCount * 2^N > max value, we need storage for N+1 buckets, each with enough
+     * slots to hold the top half of the subBucketCount (the lower half is covered by previous buckets), and the +1
+     * being used for the lower half of the 0'th bucket. Or, equivalently, we need 1 more bucket to capture the max
+     * value if we consider the sub-bucket length to be halved.
+     */
+    getLengthForNumberOfBuckets(numberOfBuckets : number): number {
+        var lengthNeeded: number = (numberOfBuckets + 1) * (this.subBucketCount / 2);
+        return lengthNeeded;
+    }
+
+    getBucketsNeededToCoverValue(value: number): number {
+        // the k'th bucket can express from 0 * 2^k to subBucketCount * 2^k in units of 2^k
+        let smallestUntrackableValue = this.subBucketCount << this.unitMagnitude;
+
+        // always have at least 1 bucket
+        let bucketsNeeded = 1;
+        while (smallestUntrackableValue <= value) {
+            if (smallestUntrackableValue > (Number.MAX_SAFE_INTEGER / 2)) { // TODO check array max size in JavaScript
+                // next shift will overflow, meaning that bucket could represent values up to ones greater than
+                // Number.MAX_SAFE_INTEGER, so it's the last bucket
+                return bucketsNeeded + 1;
+            }
+            smallestUntrackableValue <<= 1;
+            bucketsNeeded++;
+        }
+        return bucketsNeeded;
     }
 
 

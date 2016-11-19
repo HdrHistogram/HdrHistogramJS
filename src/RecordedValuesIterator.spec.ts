b388dd98ff5099c0bbe77f187c406795d1c3cc4d
alexvictoor <alexvictoor@gmail.com>
2016-11-19 16:16:12 +0100

    tests on recorded values iterator

--- src/RecordedValuesIterator.spec.ts
+++ src/RecordedValuesIterator.spec.ts
@@ -17,4 +17,25 @@ describe('Recorded Values Iterator', () => {
     expect(iterationValue.totalCountToThisValue).equals(1);
     expect(iterationValue.totalValueToThisValue).equals(12345);
   });
+
+  it("should iterate to all recorded values", () => {
+    // given
+    const histogram = new Histogram(1, Number.MAX_SAFE_INTEGER, 2);
+    histogram.recordValue(1);
+    histogram.recordValue(300);
+    histogram.recordValue(3000);
+    const iterator = new RecordedValuesIterator(histogram);
+    // when
+    const values: number[] = [];
+    while (iterator.hasNext()) {
+      values.push(iterator.next().valueIteratedTo);
+    }
+    // then
+    expect(values).to.have.length(3);
+    expect(values[0]).equals(1);
+    expect(values[1]).satisfies((value: number) => value >= 300);
+    expect(values[2]).satisfies((value: number) => value >= 3000);
+  });
+
+
 });

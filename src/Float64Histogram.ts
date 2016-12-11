4d92e4984fdb5b16c26939ef8bdd16be560c3e0a
alexvictoor <alexvictoor@gmail.com>
2016-12-11 21:29:22 +0100

    Add Float64 based histogram

---
+++ src/Float64Histogram.ts
@@ -0,0 +1,100 @@
+/*
+ * This is a TypeScript port of the original Java version, which was written by
+ * Gil Tene as described in
+ * https://github.com/HdrHistogram/HdrHistogram
+ * and released to the public domain, as explained at
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+import { AbstractHistogram } from "./AbstractHistogram";
+
+class Float64Histogram extends AbstractHistogram {
+
+  counts: Float64Array;
+  totalCount: number;
+
+  constructor(
+    lowestDiscernibleValue: number, 
+    highestTrackableValue: number, 
+    numberOfSignificantValueDigits: number) {
+
+    super(
+      lowestDiscernibleValue, 
+      highestTrackableValue, 
+      numberOfSignificantValueDigits
+    );
+    this.totalCount = 0;
+    this.counts = new Float64Array(this.countsArrayLength);
+  }
+
+  incrementCountAtIndex(index: number) {
+    const currentCount = this.counts[index];
+    const newCount = currentCount + 1;
+    if (newCount < 0) {
+      throw newCount + " would overflow short integer count";
+    }
+    this.counts[index] = newCount;
+  }
+
+  addToCountAtIndex(index: number, value: number) {
+    const currentCount = this.counts[index];
+    const newCount = currentCount + value;
+    if ((newCount < Number.MIN_SAFE_INTEGER) || (newCount > Number.MAX_SAFE_INTEGER)) {
+      throw newCount + " would overflow integer count";
+    }
+    this.counts[index] = newCount;
+  }
+
+  setCountAtIndex(index: number, value: number) {
+    if ((value < Number.MIN_SAFE_INTEGER) || (value > Number.MAX_SAFE_INTEGER)) {
+      throw value + " would overflow integer count";
+    }
+    this.counts[index] = value;
+  }
+
+  resize(newHighestTrackableValue: number) {
+    this.establishSize(newHighestTrackableValue);
+    const newCounts = new Float64Array(this.countsArrayLength);
+    newCounts.set(this.counts);
+    this.counts = newCounts;
+  }
+
+  setNormalizingIndexOffset(normalizingIndexOffset: number) {
+  }
+
+  incrementTotalCount() {
+    this.totalCount++;
+  }
+
+  addToTotalCount(value: number) {
+    this.totalCount += value;
+  }
+
+  setTotalCount(value: number) {
+    this.totalCount = value;
+  }
+
+  getTotalCount() {
+    return this.totalCount;
+  }
+
+  getCountAtIndex(index: number)  {
+    return this.counts[index];
+  }
+
+  protected _getEstimatedFootprintInBytes() {
+    return (512 + (8 * this.counts.length));
+  }
+
+  copyCorrectedForCoordinatedOmission(expectedIntervalBetweenValueSamples: number) {
+    const copy 
+      = new Float64Histogram(
+        this.lowestDiscernibleValue, 
+        this.highestTrackableValue, 
+        this.numberOfSignificantValueDigits
+      );
+    copy.addWhileCorrectingForCoordinatedOmission(this, expectedIntervalBetweenValueSamples);
+    return copy;
+  }
+}
+
+export default Float64Histogram; 

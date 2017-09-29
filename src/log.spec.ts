ebc2fb51e4e210f50c91641b8e8b4fbaf00644ad
alexvictor <alexvictor@gmail.com>
2017-09-29 23:45:26 +0200

    chore: fix formating to please prettier

--- src/log.spec.ts
+++ src/log.spec.ts
@@ -1,28 +1,33 @@
 import { expect } from "chai";
-import { HistogramLogWriter, HistogramLogReader, build, AbstractHistogram } from ".";
+import {
+  HistogramLogWriter,
+  HistogramLogReader,
+  build,
+  AbstractHistogram
+} from ".";
 
 describe("Logs", () => {
-    it("should give same result after been written the read", () => {
-        // given
-        let buffer = "";
-        const writer = new HistogramLogWriter(content => {
-          buffer += content;
-        });
-        writer.outputLogFormatVersion();
-        writer.outputStartTime(12345000);
-        writer.outputLegend();
-        const inputHistogram = build();
-        inputHistogram.recordValue(1515);
-        inputHistogram.recordValue(1789);
-        // when
-        writer.outputIntervalHistogram(inputHistogram, 12345042, 1234056, 1);
-        console.log(buffer)
-        const reader = new HistogramLogReader(buffer);
-        const outputHistogram = reader.nextIntervalHistogram();
-        // then
-        expect(outputHistogram).to.be.not.null;
-        const outputText = (outputHistogram as AbstractHistogram).outputPercentileDistribution();
-        const inputText = inputHistogram.outputPercentileDistribution(); 
-        expect(outputText).to.be.equal(inputText);
-    })
-})
+  it("should give same result after been written the read", () => {
+    // given
+    let buffer = "";
+    const writer = new HistogramLogWriter(content => {
+      buffer += content;
+    });
+    writer.outputLogFormatVersion();
+    writer.outputStartTime(12345000);
+    writer.outputLegend();
+    const inputHistogram = build();
+    inputHistogram.recordValue(1515);
+    inputHistogram.recordValue(1789);
+    // when
+    writer.outputIntervalHistogram(inputHistogram, 12345042, 1234056, 1);
+    console.log(buffer);
+    const reader = new HistogramLogReader(buffer);
+    const outputHistogram = reader.nextIntervalHistogram();
+    // then
+    expect(outputHistogram).to.be.not.null;
+    const outputText = (outputHistogram as AbstractHistogram).outputPercentileDistribution();
+    const inputText = inputHistogram.outputPercentileDistribution();
+    expect(outputText).to.be.equal(inputText);
+  });
+});

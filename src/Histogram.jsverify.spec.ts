a0d8d351c8a7960f587c317c2f5c3a8852d35fb7
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-02-26 13:23:40 +0100

    test: first attempt to use property based testing to assess histogram correctness

---
+++ src/Histogram.jsverify.spec.ts
@@ -0,0 +1,69 @@
+/*
+ * This is a TypeScript port of the original Java version, which was written by
+ * Gil Tene as described in
+ * https://github.com/HdrHistogram/HdrHistogram
+ * and released to the public domain, as explained at
+ * http://creativecommons.org/publicdomain/zero/1.0/
+ */
+import { expect } from "chai";
+import * as jsc from "jsverify";
+import * as hdr from "./index";
+
+describe("Histogram percentile computation", () => {
+  const checkOptions = {
+    rngState: "0559a70d12fe8436cb",
+    tests: 10000
+  };
+
+  it("should be accurate according to its significant figures", () => {
+    const numberOfSignificantValueDigits = 3;
+    const histogram = hdr.build({ numberOfSignificantValueDigits });
+
+    const property = jsc.check(
+      jsc.forall(arbData(100), numbers => {
+        histogram.reset();
+        numbers.forEach(n => histogram.recordValue(n));
+        const actual = quantile(numbers, 90);
+        const got = histogram.getValueAtPercentile(90);
+        const relativeError = 1 - got / actual;
+        const variation = Math.pow(10, -numberOfSignificantValueDigits);
+        return relativeError < variation;
+      }),
+      checkOptions
+    );
+    expect(property).to.be.true;
+  });
+
+  const arbData = (size: number): jsc.Arbitrary<number[]> => {
+    const replicate = (
+      n: number,
+      g: jsc.Arbitrary<number>
+    ): jsc.Arbitrary<number[]> => jsc.tuple(new Array(n).fill(g));
+
+    return (jsc as any).nonshrink(
+      replicate(
+        size,
+        jsc.oneof([
+          // we want values with a high range
+          jsc.nat(100),
+          jsc.nat(Number.MAX_SAFE_INTEGER)
+        ])
+      )
+    );
+  };
+
+  // reference implementation
+  const quantile = (inputData: number[], percentile: number) => {
+    const data = [...inputData].sort();
+    const index = percentile / 100 * (data.length - 1);
+    let result: number;
+    if (Math.floor(index) === index) {
+      result = data[index];
+    } else {
+      const i = Math.floor(index);
+      const fraction = index - i;
+      result = data[i] + (data[i + 1] - data[i]) * fraction;
+    }
+    return result;
+  };
+});

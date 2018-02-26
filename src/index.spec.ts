79d69e74de45f36fd188cb2d9d264c84c1b6f4f1
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-02-26 13:23:13 +0100

    chore: test clean up

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -5,7 +5,6 @@
  * and released to the public domain, as explained at
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
-import "core-js";
 import { expect } from "chai";
 import * as hdr from "./index";
 

943654c9e1b1d97b0db830ac18533760f57de4e9
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-03-05 14:01:04 +0100

    chore: improved release process

---
+++ release.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+rm -Rf dist
+mkdir dist
+cp -R node_modules dist/node_modules
+cp -R src dist/src
+cp *.json dist/
+cp webpack.config.js dist/webpack.config.js
+cd dist
+yarn prepublish

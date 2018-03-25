65285a3bf19dd7bcfb67cf366dfc3d7bd281d2e0
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-03-25 14:38:17 +0200

    dix release script

--- release.sh
+++ release.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
-rm -Rf dist
-mkdir dist
-cp -R node_modules dist/node_modules
-cp -R src dist/src
-cp *.json dist/
-cp webpack.config.js dist/webpack.config.js
-cd dist
+rm -Rf release
+mkdir release
+cp README.md release/README.md
+cp -R node_modules release/node_modules
+cp -R src release/src
+cp *.json release/
+cp webpack.config.js release/webpack.config.js
+cd release
 yarn prepare-publish

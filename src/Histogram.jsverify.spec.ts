85ac4905114df5c7e11f6cb3cc7a726538b76e36
Alexandre Victoor <alexandre.victoor@fluo.com>
2018-11-04 15:46:28 +0100

    Fix property based tests

--- src/Histogram.jsverify.spec.ts
+++ src/Histogram.jsverify.spec.ts
@@ -18,25 +18,24 @@ const runnerOptions = {
 describe("Histogram percentile computation", () => {
   it("should be accurate according to its significant figures", () => {
     const numberOfSignificantValueDigits = 3;
-    type BucketSize = 8 | 16 | 32 | 64;
-    [8, 16, 32, 64].forEach((bitBucketSize: BucketSize) => {
-      const histogram = hdr.build({
-        bitBucketSize,
-        numberOfSignificantValueDigits
-      });
-      fc.assert(fc.property(
-        arbData(100),
+    const histogram = hdr.build({
+      numberOfSignificantValueDigits
+    });
+    fc.assert(
+      fc.property(
+        arbData(2000),
         numbers => {
           histogram.reset();
           numbers.forEach(n => histogram.recordValue(n));
           const actual = quantile(numbers, 90);
           const got = histogram.getValueAtPercentile(90);
-          const relativeError = 1 - got / actual;
+          const relativeError = Math.abs(1 - got / actual);
           const variation = Math.pow(10, -numberOfSignificantValueDigits);
           return relativeError < variation;
         }
-      ), runnerOptions);
-    });
+      ), 
+      runnerOptions
+    );
   });
 });
 
@@ -45,23 +44,27 @@ describe("Histogram encoding/decoding", () => {
     const numberOfSignificantValueDigits = 3;
     type BucketSize = 8 | 16 | 32 | 64;
     [8, 16, 32, 64].forEach((bitBucketSize: BucketSize) => {
-      fc.assert(fc.property(
-        arbData(1), fc.double(50, 100),
-        (numbers, percentile) => {
-          const histogram = hdr.build({
-            bitBucketSize,
-            numberOfSignificantValueDigits
-          });
-          numbers.forEach(n => histogram.recordValue(n));
-          const encodedHistogram = hdr.encodeIntoBase64String(histogram);
-          const decodedHistogram = hdr.decodeFromCompressedBase64(
-            encodedHistogram
-          );
-          const actual = histogram.getValueAtPercentile(percentile);
-          const got = decodedHistogram.getValueAtPercentile(percentile);
-          return actual === got;
-        }
-      ), runnerOptions);
+      fc.assert(
+        fc.property(
+          arbData(1), 
+          fc.double(50, 100),
+          (numbers, percentile) => {
+            const histogram = hdr.build({
+              bitBucketSize,
+              numberOfSignificantValueDigits
+            });
+            numbers.forEach(n => histogram.recordValue(n));
+            const encodedHistogram = hdr.encodeIntoBase64String(histogram);
+            const decodedHistogram = hdr.decodeFromCompressedBase64(
+              encodedHistogram
+            );
+            const actual = histogram.getValueAtPercentile(percentile);
+            const got = decodedHistogram.getValueAtPercentile(percentile);
+            return actual === got;
+          }
+        ), 
+        runnerOptions
+      );
     });
   });
 });
@@ -70,7 +73,7 @@ const arbData = (size: number) => fc.array(fc.integer(1, Number.MAX_SAFE_INTEGER
 
 // reference implementation
 const quantile = (inputData: number[], percentile: number) => {
-  const data = [...inputData].sort();
+  const data = [...inputData].sort((a, b) => (a - b));
   const index = percentile / 100 * (data.length - 1);
   let result: number;
   if (Math.floor(index) === index) {

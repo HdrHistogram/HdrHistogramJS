91f6c2685b0c21e163568d2683dbcc9092b87bd5
Alex Victoor <alexvictoor@gmail.com>
2020-01-16 22:47:19 +0100

    First micro benchmarks

---
+++ bench/modulo.ts
@@ -0,0 +1,37 @@
+import { Suite } from "benchmark";
+
+const suite = new Suite("modulo");
+
+let someInteger = 0;
+
+suite
+  .add(
+    "%",
+    () => {
+      someInteger % 256;
+    },
+    {
+      setup: () => {
+        someInteger = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+      }
+    }
+  )
+  .add(
+    "&",
+    () => {
+      someInteger & 0xff;
+    },
+    {
+      setup: () => {
+        someInteger = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+      }
+    }
+  )
+  .on("complete", function() {
+    console.log("Fastest is " + this.filter("fastest").map("name"));
+    console.log({ result: this });
+  })
+  .run({
+    onCycle: () =>
+      (someInteger = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER))
+  });

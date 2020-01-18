8ab08f3b2a2674a385dd8b23543d4481ee82e338
Alex Victoor <alexvictoor@gmail.com>
2020-01-18 23:59:21 +0100

    data access bench

--- src/bench/histogram-data-access.ts
+++ src/bench/histogram-data-access.ts
@@ -1,36 +1,77 @@
 import b from "benny";
 import { build } from "../index";
 
+const randomInteger = () => Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
+const options = { initCount: 1000 };
+
 b.suite(
   "Histogram data access",
-  b.add("Int32Histogram", () => {
-    const histogram = build({ bitBucketSize: 32 });
-    return () => {
-      const someInteger = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-      histogram.recordValue(someInteger);
-    };
-  }),
-  b.add("PackedHistogram", () => {
-    const histogram = build({ bitBucketSize: "packed" });
-    return () => {
-      const someInteger = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-      histogram.recordValue(someInteger);
-    };
-  }),
-  b.add("Float64Histogram", () => {
-    const histogram = build({ bitBucketSize: 64 });
-    return () => {
-      const someInteger = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-      histogram.recordValue(someInteger);
-    };
-  }),
-  b.add("SparseArrayHistogram", () => {
-    const histogram = build({ bitBucketSize: "sparse_array" });
-    return () => {
-      const someInteger = Math.floor(Math.random() * Number.MAX_SAFE_INTEGER);
-      histogram.recordValue(someInteger);
-    };
-  }),
+  b.add(
+    "Int32Histogram",
+    () => {
+      const histogram = build({ bitBucketSize: 32 });
+      return () => {
+        histogram.recordValue(randomInteger());
+      };
+    },
+    options
+  ),
+  b.add(
+    "PackedHistogram",
+    () => {
+      const histogram = build({ bitBucketSize: "packed" });
+      return () => {
+        histogram.recordValue(randomInteger());
+      };
+    },
+    options
+  ),
+  b.add(
+    "Float64Histogram",
+    () => {
+      const histogram = build({ bitBucketSize: 64 });
+      return () => {
+        histogram.recordValue(randomInteger());
+      };
+    },
+    options
+  ),
+  b.add(
+    "SparseArrayHistogram",
+    () => {
+      const histogram = build({ bitBucketSize: "sparse_array" });
+      return () => {
+        histogram.recordValue(randomInteger());
+      };
+    },
+    options
+  ),
+  b.add(
+    "Int32Histogram eager allocation",
+    () => {
+      const histogram = build({
+        bitBucketSize: 32,
+        highestTrackableValue: Number.MAX_SAFE_INTEGER
+      });
+      return () => {
+        histogram.recordValue(randomInteger());
+      };
+    },
+    options
+  ),
+  b.add(
+    "Float64Histogram eager allocation",
+    () => {
+      const histogram = build({
+        bitBucketSize: 64,
+        highestTrackableValue: Number.MAX_SAFE_INTEGER
+      });
+      return () => {
+        histogram.recordValue(randomInteger());
+      };
+    },
+    options
+  ),
   b.complete(),
   b.save({ file: "data-access", format: "chart.html" })
 );

1f79f27adb045aa3a713b58de4f07660ed70fa63
Alex Victoor <alexvictoor@gmail.com>
2020-05-04 16:29:40 +0200

    WASM ByteBuffer

---
+++ assembly/ByteBuffer.ts
@@ -0,0 +1,102 @@
+/**
+ * Mimic Java's ByteBufffer with big endian order
+ */
+class ByteBuffer {
+  position: i32;
+
+  data: Uint8Array;
+
+  int64ArrayForConvert: Uint64Array;
+  int32ArrayForConvert: Uint32Array;
+  int8ArrayForConvertInt32: Uint8Array;
+  int8ArrayForConvertInt64: Uint8Array;
+
+  static allocate(size: i32 = 16): ByteBuffer {
+    return new ByteBuffer(new Uint8Array(size));
+  }
+
+  constructor(data: Uint8Array) {
+    this.position = 0;
+    this.data = data;
+    this.int64ArrayForConvert = new Uint64Array(1);
+    this.int32ArrayForConvert = new Uint32Array(1);
+    this.int8ArrayForConvertInt32 = Uint8Array.wrap(
+      this.int32ArrayForConvert.buffer
+    );
+    this.int8ArrayForConvertInt64 = Uint8Array.wrap(
+      this.int64ArrayForConvert.buffer
+    );
+  }
+
+  put(value: u8): void {
+    if (this.position === this.data.length) {
+      const oldArray = this.data;
+      this.data = new Uint8Array(this.data.length << 1);
+      this.data.set(oldArray);
+    }
+    unchecked((this.data[this.position] = value));
+    this.position++;
+  }
+
+  putInt32(value: u32): void {
+    if (this.data.length - this.position < 4) {
+      const oldArray = this.data;
+      this.data = new Uint8Array((this.data.length << 1) + 4);
+      this.data.set(oldArray);
+    }
+    unchecked((this.int32ArrayForConvert[0] = value));
+    this.data.set(this.int8ArrayForConvertInt32.reverse(), this.position);
+    this.position += 4;
+  }
+
+  putInt64(value: u64): void {
+    if (this.data.length - this.position < 8) {
+      const oldArray = this.data;
+      this.data = new Uint8Array((this.data.length << 1) + 8);
+      this.data.set(oldArray);
+    }
+    unchecked((this.int64ArrayForConvert[0] = value));
+    this.data.set(this.int8ArrayForConvertInt64.reverse(), this.position);
+    this.position += 8;
+  }
+
+  putArray(array: Uint8Array): void {
+    if (this.data.length - this.position < array.byteLength) {
+      const oldArray = this.data;
+      this.data = new Uint8Array(this.position + array.byteLength);
+      this.data.set(oldArray);
+    }
+    this.data.set(array, this.position);
+    this.position += array.byteLength;
+  }
+
+  get(): u8 {
+    const value = unchecked(this.data[this.position]);
+    this.position++;
+    return value;
+  }
+
+  getInt32(): u32 {
+    this.int8ArrayForConvertInt32.set(
+      this.data.slice(this.position, this.position + 4).reverse()
+    );
+    const value = unchecked(this.int32ArrayForConvert[0]);
+    this.position += 4;
+    return value;
+  }
+
+  getInt64(): u64 {
+    this.int8ArrayForConvertInt64.set(
+      this.data.slice(this.position, this.position + 8).reverse()
+    );
+    const value = unchecked(this.int64ArrayForConvert[0]);
+    this.position += 8;
+    return value;
+  }
+
+  resetPosition(): void {
+    this.position = 0;
+  }
+}
+
+export default ByteBuffer;
---
+++ assembly/__tests__/ByteBuffer.spec.ts
@@ -0,0 +1,135 @@
+import ByteBuffer from "../ByteBuffer";
+
+describe("ByteBuffer", () => {
+  it("should put value moving the position", () => {
+    // given
+    const buffer = ByteBuffer.allocate(3);
+    // when
+    buffer.put(<u8>123);
+    // then
+    expect<u8>(buffer.data[0]).toBe(123);
+    expect<i32>(buffer.position).toBe(1);
+  });
+
+  it("should resize when values overflow ", () => {
+    // given
+    const buffer = ByteBuffer.allocate(1);
+    buffer.put(<u8>123);
+    // when
+    buffer.put(<u8>42);
+    // then
+    expect<u8>(buffer.data[0]).toBe(123);
+    expect(buffer.data[1]).toBe(42);
+  });
+
+  it("should get value moving the position", () => {
+    // given
+    const buffer = ByteBuffer.allocate(1);
+    buffer.put(123);
+    buffer.resetPosition();
+    // when
+    const value = buffer.get();
+    // then
+    expect(value).toBe(123);
+    expect(buffer.position).toBe(1);
+  });
+
+  it("should put int32 value moving the position", () => {
+    // given
+    const buffer = ByteBuffer.allocate(8);
+    // when
+    buffer.putInt32(123);
+    // then
+    expect(buffer.data[3]).toBe(123);
+    expect(buffer.position).toBe(4);
+  });
+
+  it("should resize when int32 values overflow ", () => {
+    // given
+    const buffer = ByteBuffer.allocate(1);
+    // when
+    buffer.putInt32(42);
+    // then
+    expect(buffer.data[3]).toBe(42);
+    expect(buffer.position).toBe(4);
+  });
+
+  it("should get int32 value moving the position", () => {
+    // given
+    const buffer = ByteBuffer.allocate(1);
+    buffer.putInt32(123);
+    buffer.resetPosition();
+    // when
+    const value = buffer.getInt32();
+    // then
+    expect(value).toBe(123);
+    expect(buffer.position).toBe(4);
+  });
+
+  it("should put int64 value moving the position", () => {
+    // given
+    const buffer = ByteBuffer.allocate(8);
+    // when
+    buffer.putInt64(123);
+    // then
+    expect(buffer.data[7]).toBe(123);
+    expect(buffer.position).toBe(8);
+  });
+
+  it("should resize when int64 values overflow ", () => {
+    // given
+    const buffer = ByteBuffer.allocate(1);
+    // when
+    buffer.putInt64(42);
+    // then
+    expect(buffer.data[7]).toBe(42);
+    expect(buffer.position).toBe(8);
+  });
+
+  it("should get int64 value moving the position", () => {
+    // given
+    const buffer = ByteBuffer.allocate(1);
+    buffer.putInt64(u64.MAX_VALUE);
+    buffer.resetPosition();
+    // when
+    const value = buffer.getInt64();
+    // then
+    expect(value).toBe(u64.MAX_VALUE);
+    expect(buffer.position).toBe(8);
+  });
+
+  it("should copy all data when putting array", () => {
+    // given
+    const buffer = ByteBuffer.allocate(1024);
+    const array = new Uint8Array(4);
+    for (let index = 0; index < array.length; index++) {
+      array[index] = <u8>(index + 1);
+    }
+    // when
+    buffer.putArray(array);
+    // then
+    buffer.resetPosition();
+    expect(buffer.get()).toBe(1);
+    expect(buffer.get()).toBe(2);
+    expect(buffer.get()).toBe(3);
+    expect(buffer.get()).toBe(4);
+  });
+
+  it("should resize when putting array bigger than capacity", () => {
+    // given
+    const buffer = ByteBuffer.allocate(1024);
+    const array = new Uint8Array(4);
+    for (let index = 0; index < array.length; index++) {
+      array[index] = <u8>(index + 1);
+    }
+    // when
+    buffer.position = 1022;
+    buffer.putArray(array);
+    // then
+    buffer.position = 1022;
+    expect(buffer.get()).toBe(1);
+    expect(buffer.get()).toBe(2);
+    expect(buffer.get()).toBe(3);
+    expect(buffer.get()).toBe(4);
+  });
+});

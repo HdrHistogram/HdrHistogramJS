ab66e757f70a3f6711a596e736bc2303e0a23b0f
Alex Victoor <alexvictoor@gmail.com>
2020-05-26 23:26:25 +0200

    getValueAtPercentile benchmark

---
+++ src/bench/histogram-percentile.ts
@@ -0,0 +1,78 @@
+import b from "benny";
+import { build } from "../index";
+import { initWebAssembly } from "../wasm";
+initWebAssembly().then(() => {
+  const randomInteger = (max: number = Number.MAX_SAFE_INTEGER) =>
+    Math.floor(Math.random() * max);
+  const options = { initCount: 1000 };
+
+  b.suite(
+    "Histogram decoding",
+    b.add(
+      "Int32Histogram",
+      () => {
+        const histogram = build({
+          bitBucketSize: 32
+        });
+        for (let index = 0; index < 1024; index++) {
+          histogram.recordValueWithCount(randomInteger(), randomInteger(100));
+        }
+        return () => {
+          histogram.getValueAtPercentile(99);
+        };
+      },
+      options
+    ),
+
+    b.add(
+      "WASM 32B Histogram",
+      () => {
+        const histogram = build({
+          bitBucketSize: 32,
+          useWebAssembly: true
+        });
+        for (let index = 0; index < 1024; index++) {
+          histogram.recordValueWithCount(randomInteger(), randomInteger(100));
+        }
+        return () => {
+          histogram.getValueAtPercentile(99);
+        };
+      },
+      options
+    ),
+    b.add(
+      "Packed Histogram",
+      () => {
+        const histogram = build({
+          bitBucketSize: "packed"
+        });
+        for (let index = 0; index < 1024; index++) {
+          histogram.recordValueWithCount(randomInteger(), randomInteger(100));
+        }
+        return () => {
+          histogram.getValueAtPercentile(99);
+        };
+      },
+      options
+    ),
+    b.add(
+      "WASM Packed Histogram",
+      () => {
+        const histogram = build({
+          bitBucketSize: "packed",
+          useWebAssembly: true
+        });
+        for (let index = 0; index < 1024; index++) {
+          histogram.recordValueWithCount(randomInteger(), randomInteger(100));
+        }
+        return () => {
+          histogram.getValueAtPercentile(99);
+        };
+      },
+      options
+    ),
+
+    b.complete(),
+    b.save({ file: "percentile", format: "chart.html" })
+  );
+});

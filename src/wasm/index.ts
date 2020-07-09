3d0bcfa2bc99a064f2ea09c4d68b41a641067e30
Alex Victoor <alexvictoor@gmail.com>
2020-07-09 21:57:05 +0200

    fix prettier

--- src/wasm/index.ts
+++ src/wasm/index.ts
@@ -43,7 +43,7 @@ const defaultRequest: BuildRequest = {
   autoResize: true,
   lowestDiscernibleValue: 1,
   highestTrackableValue: 2,
-  numberOfSignificantValueDigits: 3,
+  numberOfSignificantValueDigits: 3
 };
 
 const remoteHistogramClassFor = (size?: BitBucketSize) =>
@@ -54,7 +54,7 @@ const destroyedWasmHistogram = new Proxy(
   {
     get: function(obj, prop) {
       throw new Error("Cannot use a destroyed histogram");
-    },
+    }
   }
 );
 
